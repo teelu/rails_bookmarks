1dc0f244171d99277895b25f0a6a17170e83d512
Alex Grant <grantovich@users.noreply.github.com>
2014-06-29 17:06:49 -0400

    User can view bookmarks and filter by category

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,11 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.where(category: selected_category).order(:title)
+  end
+
+  private
+
+  def selected_category
+    params[:category].presence || Bookmark::CATEGORIES
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :bookmarks
   root 'bookmarks#index'
 end
