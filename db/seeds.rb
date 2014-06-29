d2c28e1ceb9a620490dc2b31bacf477763769ff3
Alex Grant <grantovich@users.noreply.github.com>
2014-06-29 16:53:40 -0400

    Add some basic seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bookmark.delete_all
+
+Bookmark.create!(
+  url: 'http://reddit.com',
+  title: 'Reddit, the front page of the internet',
+  category: 'Funny',
+  is_favorite: true
+)
+
+Bookmark.create!(
+  url: 'http://arstechnica.com',
+  title: "All the tech news that's fit to print",
+  category: 'Useful',
+  is_favorite: true,
+  comment: 'Probably my favorite news site of all time'
+)
+
+Bookmark.create!(
+  url: 'http://hackertyper.net',
+  title: 'Type like in movies and look super productive',
+  category: 'Funny'
+)
+
+Bookmark.create!(
+  url: 'http://www.catb.org/jargon/html/',
+  title: 'Jargon File, a reference for hacker culture',
+  category: 'Serious',
+  comment: 'Keep the switch flipped to "more magic"'
+)
