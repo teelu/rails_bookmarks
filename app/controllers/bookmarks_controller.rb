7a8740caeeaa3479ca59e2e3ec8fd5a78920d719
David Fisher <tibbon@gmail.com>
2014-10-08 12:37:12 -0400

    Created destroy method. Refactored code for a shared find_bookmark method between the show, destroy, update and edit actions

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,38 @@
 class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: [:show, :edit, :update, :destroy]
+
   def index
     @bookmarks = Bookmark.order(:title)
   end
 
   def show
-    @bookmark = Bookmark.find params[:id]
   end
 
   def new
     @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url, :comment, :favorite)
   end
 end
