0d2a2967b26cea63be5d2b77cfbd6996c849e7ce
teelu <terrence.lun@gmail.com>
2015-08-18 10:44:50 +0800

    feat - validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ActiveRecord::Base
+  validates :url, presence: true, format: {with: /http:\/\//} 
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: %w(Useful Serious Funny)
+
 end
